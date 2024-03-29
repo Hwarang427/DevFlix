cfaa41554788ec3acbd1af0394621bff6b406329
Devin Rose <devinrose427@gmail.com>
2024-03-28 22:33:03 -0600

    Updated form on 3/28/24

--- register.php
+++ register.php
@@ -6,7 +6,20 @@
 
         <body><div class="signInContainer">
             <div class="column">
+
+            <form method="POST"> 
+                <input type ="text" name = "firstName" placeholder = "First Name" required>
+                <input type ="text" name = "lastName" placeholder = "Last Name" required>
+                <input type ="text" name = "userName" placeholder = "Username" required>
+                <input type ="email" name = "email" placeholder = "Email" required>
+                <input type ="email" name = "email2" placeholder = "Confirm Email" required>
+                <input type ="password" name = "password" placeholder = "Password" required>
+                <input type ="password" name = "password2" placeholder = "Confirm Password" required>
+                <input type ="submit" name = "submitPassword" value = "Submit" required>
+
                 
+
+            </form>
             </div>
 
         </div>
