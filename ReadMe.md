e4c0c19a65ea594a11ae49b97f5cbdfde4ded75f
Devin Rose <devinrose427@gmail.com>
2024-03-28 22:17:08 -0600

    r

--- ReadMe.md
+++ ReadMe.md
@@ -1,3 +1,5 @@
 ReadMe.md
 
 This is a ReadMe file for a Netflix Clone while I figure out git
+
+Having problems with GitLens, Going to complain here until it works
---
+++ register.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Welcome to DevFlix</title>
+        <link rel="stylesheet" type="text/css" href="Assets/Style/style.css"/>
+
+        <body><div class="signInContainer">
+            <div class="column">
+                
+            </div>
+
+        </div>
+            <?
+            ?>
+        </body>
+    </head>
+
+</html>
