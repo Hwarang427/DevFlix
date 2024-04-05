e5343f696ea6877312bd19786177275b0fa390fa
Devin Rose <devinrose427@gmail.com>
2024-04-05 16:48:29 -0600

    Adds Login Page and Styles both Registration and Login

--- logIn.php
+++ logIn.php
@@ -15,26 +15,23 @@
             
             <div class="header">
                 <img src="Assets/Images/3def453e5906fed4f3e38cf791e6cb6b.png" title="Logo" alt= "Site Logo"/>
-                <h3> Sign Up</h3>
+                <h3> Sign In</h3>
                 <span>to continue to DevFlix</span>
                 
 
             </div>
 
             <form method="POST"> 
-                <input type ="text" name = "firstName" placeholder = "First Name" required>
-                <input type ="text" name = "lastName" placeholder = "Last Name" required>
                 <input type ="text" name = "userName" placeholder = "Username" required>
-                <input type ="email" name = "email" placeholder = "Email" required>
-                <input type ="email" name = "email2" placeholder = "Confirm Email" required>
+               
                 <input type ="password" name = "password" placeholder = "Password" required>
-                <input type ="password" name = "password2" placeholder = "Confirm Password" required>
+                
                 <input type ="submit" name = "submitButton" value = "Submit" required>
 
                 
 
             </form>
-            <a href="login.php" class="signInMessage">Already have an Account? Sign in here!</a>
+            <a href="register.php" class="signInMessage">Need an Account? Sign Up Here!</a>
             </div>
 
         </div>
